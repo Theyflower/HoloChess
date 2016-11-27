36d857fe7e471b0c920237d04eac5a20c14642d3
Aaron Thomas <1d20m6@gmail.com>
2016-11-27 09:58:27 -0500

    Added future design specification for Piece class

--- src/piece.py
+++ src/piece.py
@@ -1,3 +1,5 @@
 def empty():
 	'''returns a piece to represent an empty tile, probably just none'''
+	#@todo(aaron) make this function return a piece representing the empty tilestate
+	#				probably returned by the 'default' constructor
 	return None
