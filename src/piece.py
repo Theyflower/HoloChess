8748f275c19726b0412dec8b7deb145d02df4325
Aaron Thomas <1d20m6@gmail.com>
2016-11-28 11:38:08 -0500

    Added implementation of empty Piece
    Piece has a contents variable, same name as Tile's contents variables.
    Tile.contents.contents is really ugly, need to rename one of those
    variables.

--- src/piece.py
+++ src/piece.py
@@ -1,5 +1,8 @@
 def empty():
-	'''returns a piece to represent an empty tile, probably just none'''
-	#@todo(aaron) make this function return a piece representing the empty tilestate
-	#				probably returned by the 'default' constructor
-	return None
+	'''returns a piece to represent an empty tile'''
+	return Piece()
+
+
+class Piece:
+	def __init__(self, contents=None):
+		self.contents = contents
