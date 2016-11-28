fa68e8fd1b6c8a91fe0b769ffa1ea4b43ee4615f
Aaron Thomas <1d20m6@gmail.com>
2016-11-28 11:54:02 -0500

    Implemented DejarikPiece and CourtierPiece

--- src/piece.py
+++ src/piece.py
@@ -1,8 +1,35 @@
+from random import randrange
+
 def empty():
 	'''returns a piece to represent an empty tile'''
 	return Piece()
 
 
 class Piece:
-	def __init__(self, contents=None):
-		self.contents = contents
+	name
+	defence
+	player
+	isking
+	def __init__(self, name="EmptyPiece", player=0, movement=0):
+		self.name = name
+		self.player = player
+		self.movement = movement
+
+class CouriterPiece(Piece):
+	def __init__(self, name="EmptyPiece", player=0, movement=0, isking=False):
+		Piece.__init__(self, name, player, movement)
+		self.isking = isking
+
+class DejarikPiece(Pice):
+	def __init__(self, name="EmptyPiece", player=0, movement=0, attack=0, defence=0):
+		Piece.__init__(self, name, player, movement)
+		self.attack = attack
+		self.defence = defence
+
+	def attack_roll(self, numdice):
+		'''returns the result of rolling xdy where x is the number of dice and y is the attack'''
+		return randrange(1,attack) * numduce
+
+	def defence_roll(self, numdice):
+		'''returns the result of rolling xdy where x is the number of dice and y is the defence'''
+		return randrange(1,defence) * numdice
